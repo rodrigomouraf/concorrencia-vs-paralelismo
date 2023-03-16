daddf9d6cbe912987f583b7989b7741cd78a34ca
Rodrigo <rodrigomoura_rm@hotmail.com>
2023-03-16 20:18:25 -0300

    feat: change sync to channel

--- go/comprar-bolo.go
+++ go/comprar-bolo.go
@@ -1,37 +1,53 @@
 package main
 
+//Apresentar com Pedro a 2 e 9 segundos
+
 import (
 	"fmt"
 	"sync"
 	"time"
+	"runtime"
 )
 
 var wg sync.WaitGroup
 
 func main() {
-	fruta := make(chan string)
-	ovos := make(chan string)
+	fmt.Println("Número de processadores inicio: ", runtime.NumCPU())
+	fmt.Println("Número de coroutine inicio: ", runtime.NumGoroutine())
 
-	go comprarOvo(ovos)
-	go comprarFrutas(fruta)
-	wg.Wait()
-	fmt.Println("Maria vai fazer bolo de", <-fruta)
+	buscarFruta := make(chan string)
+	buscarOvos := make(chan string)
+
+	go comprarOvos(buscarOvos)	
+	go comprarFrutas(buscarFruta)	
+
+	fmt.Println("Número de coroutine meio: ", runtime.NumGoroutine())
+
+	select {
+		case <-buscarOvos:
+			iniciarPreparo()
+    }
+
+	fmt.Println("Maria vai fazer bolo de", <-buscarFruta)
+	
 }
 
-func comprarOvo(ovos chan string) {
-	wg.Add(1)
+func iniciarPreparo(){
+    fmt.Println("Maria está começando a preparar a massa");
+    time.Sleep(3 * time.Second)
+    fmt.Println("Maria precisa do ingrediente da Joaquina");
+}
+
+func comprarOvos(buscarOvos chan string) {
 	fmt.Println("Pedro chegou no mercado")
 	time.Sleep(2 * time.Second)
 	fmt.Println("Pedro comprou ovos")
-	ovos <- "comprei ovos"
-	wg.Done()
+	buscarOvos <- "comprei ovos"
 }
 
-func comprarFrutas(fruta chan string) {
-	wg.Add(1)
+func comprarFrutas(buscarFruta chan string) {
 	fmt.Println("Joaquina chegou na frutaria")
 	time.Sleep(4 * time.Second)
 	fmt.Println("Joaquina comprou uva")
-	fruta <- "uva"
-	wg.Done()
+	buscarFruta <- "uva"
 }
